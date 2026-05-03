ed923243a928f4abb91a9aafa187800419f8c6e4
CodeNika <145202239+CodeNikaa@users.noreply.github.com>
2026-05-03 20:39:06 +0200

    connected Frontend to Backend + changed render from hc object of arrays to render from db

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ dotenv.config();
 import './db.js';
 import router from './routes/cars.js';
 import express from 'express';
+import path from 'path';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.json());
+app.use(express.static('../'))
 app.use('/api/cars', router);
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+
+    res.sendFile('index.html');
 })
 
 app.listen(PORT, () => {
