5a4363df3045a63d9800423e9230138dec0e58e5
CodeNika <145202239+CodeNikaa@users.noreply.github.com>
2026-05-03 20:06:45 +0200

    finshed CRUD routes for the app

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { json } from 'express';
 import Car from '../models/Car.js';
 
 const router = express.Router();
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
     const autos = await Car.find();
 
     res.json(autos);
-})
+});
 
 router.post('/', async (req, res) => {
     try {
@@ -26,6 +26,28 @@ router.post('/', async (req, res) => {
         console.log(error);
         res.status(500).json({message: error.message});
     }
-})
+});
+
+router.delete('/:id', async (req, res) => {
+
+    try {
+        const getCar = await Car.findByIdAndDelete(req.params.id);
+        res.status(200).json({message: "Auto erfolgreich gelöscht"});
+    } catch (err) {
+        console.log('id is missing or corrupt', err);
+        res.status(500).json({message: err.message});
+    }
+});
+
+router.put('/:id', async (req, res) => {
+
+    try {
+        const getCar = await Car.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json({message: "Auto erfolgreich geändert", auto: getCar});
+    } catch (err) {
+        console.log('id is incorrect or missing');
+        res.status(500).json({message: err.message});
+    }
+});
 
 export default router;
